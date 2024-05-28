aeba01fc2aacac11383117d2f8839154c302b1b9
Parag Datar <parag.datar7777@gmail.com>
2024-05-28 15:31:48 -0700

    Remove unused element

--- app/rag_url/page.tsx
+++ app/rag_url/page.tsx
@@ -1,66 +1,13 @@
 import { ChatWindow } from "@/components/ChatWindow";
 
 export default function QuickStart() {
- 
-  const InfoCard = (
-    <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
-      <h1 className="text-3xl md:text-4xl mb-4">
-        ▲ Next.js + LangChain.js 🦜🔗
-      </h1>
-      <ul>
-        <li className="text-l">
-          🤝
-          <span className="ml-2">
-            This template showcases a simple chatbot using{" "}
-            <a href="https://js.langchain.com/" target="_blank">
-              LangChain.js
-            </a>{" "}
-            and the Vercel{" "}
-            <a href="https://sdk.vercel.ai/docs" target="_blank">
-              AI SDK
-            </a>{" "}
-            in a{" "}
-            <a href="https://nextjs.org/" target="_blank">
-              Next.js
-            </a>{" "}
-            project.
-          </span>
-        </li>
-        <li className="hidden text-l md:block">
-          💻
-          <span className="ml-2">
-            You can find the prompt and model logic for this use-case in{" "}
-            <code>app/api/chat/quick_start/route.ts</code>.
-          </span>
-        </li>
-        <li>
-          <span className="ml-2">
-            It is a well behaved bot that is polite and direct.️
-          </span>
-        </li>
-        <li className="hidden text-l md:block">
-          🎨
-          <span className="ml-2">
-            The main frontend logic is found in <code>app/page.tsx</code>.
-          </span>
-        </li>
-        
-        <li className="text-l">
-          <span className="ml-2">
-          This system is configured chat with JS MDN does. Click `Prime the system button` to ingest MDN docs. Once you see success message start chatting with it.
-          </span>
-        </li>
-      </ul>
-    </div>
-  );
   return (
     <>
       <ChatWindow
         endpoint="api/chat/rag_url"
         titleText="RAG with URLs"
         placeholder="As questions about the content of the upload URL?"
-        emptyStateComponent={InfoCard}
-        showModelOptions={true}
+        // showModelOptions={true}
         showURLIngestForm={true}
       ></ChatWindow>
     </>
